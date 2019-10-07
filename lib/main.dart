5b84f855905775e5d32969a845991f09d64ad031
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-07 13:13:21 +0530

    play pause functionality  added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:spotify_player_clone/song.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
+
   runApp(MaterialApp(
       home: SpotifyPlayer(),
   ));
@@ -21,10 +22,27 @@ class _SpotifyPlayerState extends State<SpotifyPlayer> {
       'https://i.scdn.co/image/f218335b215402cc2fb3b8d92652ebad48458805',
       'Luka Chuppi');
   List<Song> allSongs = SongData().songs; // You are given a list of songs here for Stretch
+  AudioPlayer audioPlayer = AudioPlayer();
+  bool playing=false;
+  triggerAudioPlayer()
+  {
+    setState(() {
+      if(!playing)
+      {
+        playing=true;
+        audioPlayer.play(dummySong.playUrl);
+      }
+      else{
+        playing=false;
+        audioPlayer.pause();
+      }
+    });
 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(
           'Best of Hindi',
@@ -33,18 +51,28 @@ class _SpotifyPlayerState extends State<SpotifyPlayer> {
 
       ),
       body: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Image.network('https://i.scdn.co/image/f218335b215402cc2fb3b8d92652ebad48458805'),
-            Text(dummySong.name),
-            Text(dummySong.artistName),
+            Text(dummySong.name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.white),textAlign: TextAlign.justify,),
+            Text(dummySong.artistName,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 10,color: Colors.white)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                IconButton(icon:Icon(Icons.thumb_up),onPressed: (){},),
-                IconButton(icon:Icon(Icons.skip_previous),onPressed: (){},),
-                IconButton(icon:Icon(Icons.play_arrow),onPressed: (){},iconSize: 50,),
-                IconButton(icon:Icon(Icons.skip_next),onPressed: (){},),
-                IconButton(icon:Icon(Icons.thumb_down),onPressed: (){},),
+                IconButton(icon:Icon(Icons.thumb_up),onPressed: (){},color: Colors.white,),
+                IconButton(icon:Icon(Icons.skip_previous),onPressed: (){},color: Colors.white,),
+                FloatingActionButton(
+                        onPressed: ()
+                         {
+                           triggerAudioPlayer();
+
+                        },
+
+                        child: playing?new Icon(Icons.pause):new Icon(Icons.play_arrow)),
+                IconButton(icon:Icon(Icons.skip_next),onPressed: (){},color: Colors.white,),
+                IconButton(icon:Icon(Icons.thumb_down),onPressed: (){},color: Colors.white,),
+
               ],
             )
 
